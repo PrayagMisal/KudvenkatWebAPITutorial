47129fd438b1026c796d96d453754afdf7d5e26c
Prayag Misal <prayagmisal@gmail.com>
2022-09-20 22:31:51 +0530

    addition of employee database entity and user registration implementation

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        EmployeeDBEntities entities = new EmployeeDBEntities();
+        public IEnumerable<Employee> Get()
+        {
+            return entities.Employees.ToList();
+        }
+    }
+}
